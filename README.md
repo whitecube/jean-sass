d6e4a0c3642d69b2354390bf53b0141877c37337
Jonathan Rixhon <jonathan.rixhon@hotmail.com>
2025-02-18 15:40:50 +0100

    add parts to stubs and add mini readme to install

--- README.md
+++ README.md
@@ -1,55 +1,29 @@
-white-sass
-==========
+# white-sass
 
-A few SASS mixins and functions we use on daily basis
+## Get started
 
-## Update 05/02/2018
+First add `white-sass` to your package.json's dependencies.
 
-Reworked some of the font helpers and added a color mixin.
+```json
+{
+  "dependencies": {
+    "@whitecube/white-sass": "git@github.com:whitecube/white-sass.git" //#branch-name if needed
+  }
+}
+```
 
-**Fonts & Sizing**  
+Then, run `npm install`. Once it's done, to use it you can either, run the command follwing command :
 
-### `font(family, variant)`  
-Adds the necessary css rules to use the requested font/variant combo. These values are defined beforehand in the `$ws_fonts-path` variable (`config/_fonts.scss`)
+```sh
+npm white-sass install
+```
 
-### `em(goal, parent)`  
-Returns a `em` value for the given parameters, which can either be a font-size identifier (defined in `config/_typography`) or a numeric value (expressed in pixels).
+or use it as a more classical way by using it in your files:
 
-### `px(goal)`  
-Returns a `px` value for the given parameters, which can either be a font-size identifier (defined in `config/_typography`) or a numeric value (expressed in pixels).
-
-### `fvalue(font-size)`  
-Returns an unit-less pixel value for the given font-size identifier (defined in `config/_typography`).
-
-**Colors**  
-### `color(color)`  
-Returns the color code defined in the `$ws_colors` variable (`config/_colors.scss`)
-
-## Update 11/12/2015
-
-Added **Kaduk**, a mixin (using independent functions if you want more flexibility) that allows you to do this (and it works in responsive if you pass it a percentage value) :
-
-![schema of Kaduk ](https://raw.githubusercontent.com/whiteCube/white-sass/master/kaduk.png)
-
-## Update 11/11/2015
-
-Added the following features :
-
-- Gridz: `fixedCol()`: You can now get grid values returned in `px`, `em`, `rem` or `bananas` instead of `%`.
-- Utils:
-      - `cover()`: Set an element's position in order to cover its relative parent.
-      - `rmUnit()`: Removes units from values (Different from Sass's `unitless()`, which only returns `true` or `false`)
-      - `clickableTransparentBg()`: Easy bug fix for IE on transparent links displayed as blocks.
-- Sprites: `@include setHalfSpriteBack()`: does exactly what it says
-- **Icons !** You can now easily use icon-fonts and add pink SVG hearts everywhere with a simple mixin !
-
-
-Updated :
-
-- Config file is now easier to use.
-- pseudoElements: triangles can now be used on any element (but it's always better on a `::before` or `::after`) and has all the possible directions (topLeft, top, topRight, right, bottomRight, bottom, bottomLeft, left).
-- Sprites: added retina support (we actually used it for a while but never added it to this repo).
-
-Removed :
-
-- CompassOverride. Was not pertinent anymore.
+```scss
+@use "@whitecube/white-sass";
+@use "@whitecube/white-sass/tools" as *;
+body {
+  font-size: rem(1000);
+}
+```
--- bin/commands/install.js
+++ bin/commands/install.js
@@ -1,69 +1,69 @@
-const readline = require('node:readline');
-const fs = require('fs');
-const path = require('path');
+const readline = require("node:readline");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Install {
-    constructor(process, whiteSassPath) {
-        this.process = process
-        this.srcPath = `${whiteSassPath}/src`;
+  constructor(process, whiteSassPath) {
+    this.process = process;
+    this.srcPath = `${whiteSassPath}/src`;
 
-        this.destination = null;
-        this.stubs = {
-            app: `${this.srcPath}/stubs/sass/app.scss`,
-            config: `${this.srcPath}/stubs/sass/config`
-        }
+    this.destination = null;
+    this.stubs = {
+      app: `${this.srcPath}/stubs/sass/app.scss`,
+      parts: `${this.srcPath}/stubs/sass/parts/_parts.scss`,
+      config: `${this.srcPath}/stubs/sass/config`,
+    };
+  }
+
+  setDestination(destinationPath) {
+    this.destination = {
+      app: `${destinationPath}/app.scss`,
+      parts: `${destinationPath}/parts/_parts.scss`,
+      config: `${destinationPath}/config`,
+    };
+  }
+
+  run() {
+    this.checkForPath((input) => {
+      let destinationPath = path.normalize(`${this.process.env.PWD}/${input}`);
+      this.install(destinationPath);
+    });
+  }
+
+  checkForPath(then) {
+    if (!process.argv[3]) {
+      return this.askForPath(then);
     }
 
-    setDestination(destinationPath) {
-        this.destination = {
-            app: `${destinationPath}/app.scss`,
-            config: `${destinationPath}/config`
-        }
-    }
+    then(this.process.argv[3]);
+  }
 
-    run() {
-        this.checkForPath((input) => {
-            let destinationPath = path.normalize(`${this.process.env.PWD}/${input}`)
-            this.install(destinationPath);
-        })
-    }
+  askForPath(then) {
+    const question = `Please specify the resource path (example: resource/sass) \n`;
 
-    checkForPath(then) {
-        if (!process.argv[3]) {
-            return this.askForPath(then);
-        }
+    const rl = readline.createInterface({
+      input: this.process.stdin,
+      output: this.process.stdout,
+    });
 
-        then(this.process.argv[3]);
-    }
+    rl.question(question, (input) => {
+      then(input);
+      rl.close();
+    });
+  }
 
-    askForPath(then) {
-        const question = `Please specify the resource path (example: resource/sass) \n`;
+  install(destinationPath) {
+    this.setDestination(destinationPath);
+    Object.keys(this.stubs).forEach((key) => {
+      this.copy(this.stubs[key], this.destination[key]);
+    });
+  }
 
-        const rl = readline.createInterface({
-            input: this.process.stdin,
-            output: this.process.stdout,
-        });
+  async copy(source, destination) {
+    await fs.cp(source, destination, { recursive: true }, (error) => {
+      let message = error ? error : `${source} => ${destination} \n`;
 
-        rl.question(question, input => {
-            then(input);
-            rl.close();
-        });
-    }
-
-    install(destinationPath) {
-        this.setDestination(destinationPath)
-        Object.keys(this.stubs).forEach(key => {
-            this.copy(this.stubs[key], this.destination[key]);
-        });
-    }
-
-    async copy(source, destination) {
-        await fs.cp(source, destination, { recursive: true }, (error) => {
-            let message = error
-                ? error
-                : `${source} => ${destination} \n`
-
-            console.log(message);
-        });
-    }
-}
+      console.log(message);
+    });
+  }
+};
